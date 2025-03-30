47e59357aff4e27ce2b4f5c7983d18a88fe3cba2
greyw01f <83853649+greyw01f@users.noreply.github.com>
2025-03-30 17:36:07 -0300

    Delete assets/js/ejercicio.js

--- assets/js/ejercicio.js
+++
@@ -1,6 +0,0 @@
-//Seleccionar el elemento
-fotoPerfil =document.querySelector("#foto-perfil")
-//Solicitar a la persona la personalización
-radioFoto = prompt("Ingresa un valor para poder redondear tu foto de perfil, por ejemplo 10px, 20%")
-//Aplicar el estilo
-fotoPerfil.style.borderRadius = radioFoto
