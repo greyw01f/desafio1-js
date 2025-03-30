68693aefd65a259f542e0fe36a5e5aa059a38856
greyw01f <83853649+greyw01f@users.noreply.github.com>
2025-03-30 17:34:54 -0300

    Add files via upload

---
+++ assets/js/ejercicio.js
@@ -0,0 +1,6 @@
+//Seleccionar el elemento
+fotoPerfil =document.querySelector("#foto-perfil")
+//Solicitar a la persona la personalización
+radioFoto = prompt("Ingresa un valor para poder redondear tu foto de perfil, por ejemplo 10px, 20%")
+//Aplicar el estilo
+fotoPerfil.style.borderRadius = radioFoto
